08e8891357cb96282d64583fcfd77eab19545ca0
Makinay <vitor_souza97@hotmail.com>
2022-08-23 22:01:33 -0300

    Routes index.js feito

---
+++ api/Routes/index.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser')
+
+const fornecedor = require('./fornecedorRoute')
+const login = require('./loginRoute')
+const pedido = require('./pedidoRoute')
+const produto = require('./produtoRoute')
+const usuario = require('./usuarioRoute')
+
+module.exports = app => {
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded( {extended: false}))
+    app.use(fornecedor)
+    app.use(login)
+    app.use(pedido)
+    app.use(produto)
+    app.use(usuario)
+    
+} 
